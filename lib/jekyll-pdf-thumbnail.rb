fa01711ba5d2f0d535bf3fb8c6a289c74dffb07b
Noe Nieto <nnieto@noenieto.com>
2024-04-08 14:56:06 -0700

    Remove debug gem

--- lib/jekyll-pdf-thumbnail.rb
+++ lib/jekyll-pdf-thumbnail.rb
@@ -1,6 +1,5 @@
 require 'pdftoimage'
 require 'jekyll'
-require 'debug'
 require "digest"
 
 
--- spec/jekyll-pdf-thumbnail_spec.rb
+++ spec/jekyll-pdf-thumbnail_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require 'debug'
+
 
 describe "JekyllPDFThumbnail" do
     let(:overrides) { Hash.new }
